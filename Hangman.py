48e4c2f85d9c5b7c0ecbb5f0f36964d315653da4
jcavb <jcavilasboas@gmail.com>
2025-02-25 00:14:53 -0300

    Win condition done

--- Hangman.py
+++ Hangman.py
@@ -11,19 +11,32 @@ for letter in chosen_word:
     placeholder += "_"
 print(placeholder)
 
-guess = input("Guess a letter: ").lower()
 
 
-display = ""
 
-for letter in chosen_word:
-    if guess == letter:
-        display += guess
-    else:
-        display += "_"
 
-print(display)
+game_over = False
+correct_letter = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+    
+    display = ""    
+    for letter in chosen_word:
+        if guess == letter:
+            display += guess
+            correct_letter.append(guess)
+            
+        elif letter in correct_letter:
+            display += letter
+            
+        else:
+            display += "_"
+    
+    print(display)
+    
+    if "_" not in display:
+        print("You Win!! :)")
+        game_over = True
 
         
-            
-

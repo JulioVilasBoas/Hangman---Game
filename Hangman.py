452f5de13738e03b75876f9ba5a6f19efffdbc5e
jcavb <jcavilasboas@gmail.com>
2025-02-24 23:37:50 -0300

    Display and placeholder done

--- Hangman.py
+++ Hangman.py
@@ -1,5 +1,4 @@
 import random
-# TO DO 1 Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print
 
 word_list = ['aardvark', 'baboon', 'camel']
 
@@ -7,27 +6,23 @@ chosen_word = random.choice(word_list)
 
 print(chosen_word)
 
-# TO DO 2: Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase
+placeholder = ""
+for letter in chosen_word:
+    placeholder += "_"
+print(placeholder)
 
 guess = input("Guess a letter: ").lower()
 
-# TO DO 3: Check if the letter the user guessed is one of the letters in the chosen_word. Print right if it is, wrong it is not.
 
-
-# for letter in chosen_word:
-#     if guess == letter:
-#         print("Right")
-        
-    
-#     else:
-#         print("Wrong")
-
-# TO DO 4: Create a placeholder with the same number of blanks as the chosen word and fill it with "_" 
-
-placeholder = ""
+display = ""
 
 for letter in chosen_word:
-    placeholder += "_"
+    if guess == letter:
+        display += guess
+    else:
+        display += "_"
+
+print(display)
 
         
             
